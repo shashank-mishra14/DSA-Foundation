cf0028fc44e61554d03461ec476da857e280cc74
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-04-06 15:22:23 +0530

    BenjaminBulbs.java

---
+++ Benjamin Bulbs.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+public static void main(String[] args) {
+ // write your code here
+ Scanner scn = new Scanner(System.in);
+ int n = scn.nextInt();
+ for(int i = 1; i * i <= n; i++){
+    System.out.println(i * i);
+ }}} 
