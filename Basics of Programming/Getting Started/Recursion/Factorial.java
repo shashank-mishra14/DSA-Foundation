f3783f06e45f470e4e19802c7b5239f508d739b1
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-05-02 01:33:23 +0530

    Factorial.java

---
+++ Basics of Programming/Getting Started/Recursion/Factorial.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        
+        int n=scn.nextInt();
+        int f=factorial(n);
+        
+        System.out.println(f);
+    }
+
+    public static int factorial(int n){
+        
+        if(n==1)
+        {
+            return 1;
+        }
+        int fnml=factorial(n-1);
+        int fn=n*fnml;
+        return fn;
+        
+    }
+
+}
