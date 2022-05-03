de23a45c82ad83f2f86464d4d364f01c8effff3f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-05-03 12:16:28 +0530

    PowerLinear.java

---
+++ Basics of Programming/Getting Started/Recursion/PowerLinear.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        int n=scn.nextInt();
+        
+        int xn=power(x,n);
+        System.out.println(xn);
+        
+    }
+
+    public static int power(int x, int n){
+        
+        if(n==0)
+        {
+            return 1;
+            
+        }
+        int xnml=power(x, n-1);
+        
+        int xn=x*xnml;
+        return xn;
+        
+    }
+
+}
